66571c36848cdadcd727400713022ec382493b84
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-01-23 00:59:13 +0100

    Add Photo component and wire in the first reducer

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                I'm the photo-grid
+                {this.props.posts.map((post, index) => <Photo {...this.props} key={index} post={post} i={index} />)}
             </div>
         )
     }
