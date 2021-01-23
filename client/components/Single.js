f2bfa4dc9309929a127b9f816ed2a6db0f62461e
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-01-23 02:53:20 +0100

    Add Comments component and add comments to Single component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        const { posts, params, comments } = this.props;
+        const i = posts.findIndex((post) => post.code === params.postId);
+        const post = posts[i];
+        const postComments = comments[params.postId] || [];
         return (
             <div className="single-photo">
-                I'm the single-photo
+                <Photo i={i} post={post} {...this.props} />
+                <Comments postComments={postComments} />
             </div>
         )
     }
